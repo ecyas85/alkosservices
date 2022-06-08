4d099fa412ad44421b76dde905002c80a575122f
ecyas85 <ecyas85@gmail.com>
2022-06-09 01:06:16 +0300

    - Add CustomerConfig to customer service - Add call to fraud service via rest template

--- customer/src/main/java/com/alkoscode/customer/service/CustomerService.java
+++ customer/src/main/java/com/alkoscode/customer/service/CustomerService.java
@@ -1,12 +1,15 @@
 package com.alkoscode.customer.service;
 
 import com.alkoscode.customer.CustomerRegistrationRequest;
-import com.alkoscode.customer.repository.CustomerRepository;
+import com.alkoscode.customer.FraudCheckResponse;
 import com.alkoscode.customer.model.Customer;
+import com.alkoscode.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(CustomerRepository customerRepository,
+                              RestTemplate restTemplate) {
     public void registerCustomer(final CustomerRegistrationRequest request) {
 
         Customer customer = Customer.builder()
@@ -15,10 +18,19 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .email(request.email())
                 .build();
 
+        customerRepository.saveAndFlush(customer);
         //todo: check if email valid
         //todo: check if email not taken
         //todo: check if fraudster
-        customerRepository.save(customer);
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if (fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("Fraudster");
+        }
         //todo: send a notification
     }
 }
